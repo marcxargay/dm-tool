87ddcfac5a65063488d9968455afbbc91e4a90a8
Marc Xargay <marcxargay@gmail.com>
2022-03-30 13:28:09 +0200

    add prettier and interfaces

---
+++ interfaces/IDice.tsx
@@ -0,0 +1,13 @@
+export default interface IDice {
+  type: DiceType,
+  maxValue: number
+}
+
+export enum DiceType {
+  d4 = 'd4',
+  d6 = 'd6',
+  d8 = 'd8',
+  d10 = 'd10',
+  d12 = 'd12',
+  d20 ='d20',
+}
---
+++ interfaces/IWepaon.tsx
@@ -0,0 +1,5 @@
+export default interface IWeapon {
+  name: string
+  attackBonus: string
+  damage: string
+}
